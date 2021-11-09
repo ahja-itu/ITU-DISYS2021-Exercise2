6ec2daf22dc7dfb41aaab728e7d9c0a4dd26f01d
Andreas Wachs <andreas@wachs.dk>
2021-11-09 10:59:59 +0000

    Big sinful commit
    Co-authored-by: Joachim Borup <JoachimBorup@users.noreply.github.com>
    Co-authored-by: Adrian Borup <avborup@users.noreply.github.com>
    Co-authored-by: AMB-F <AMB-F@users.noreply.github.com>

---
+++ src/clock.go
@@ -0,0 +1,37 @@
+package main
+
+import "sync"
+
+type Clock struct {
+	count uint64
+	lock  sync.Mutex
+}
+
+func NewClock() Clock {
+	return Clock{count: 0}
+}
+
+func (c *Clock) Increment() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	c.count++
+}
+
+func (c *Clock) GetCount() uint64 {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	return c.count
+}
+
+func (c1 *Clock) Update(c2 uint64) {
+	c1.lock.Lock()
+	defer c1.lock.Unlock()
+
+	if c1.count > c2 {
+		c1.count++
+	} else {
+		c1.count = c2 + 1
+	}
+}
